cf22e42c23e3b87bbd1198dd91abb61b0ab05da3
Robyn <wrenwreu@gmail.com>
2024-03-01 12:54:13 -0500

    Parsing support for basic var statements.

--- source/parser.d
+++ source/parser.d
@@ -5,6 +5,7 @@ import ast;
 import context;
 import error;
 
+// Policy is to increment at the start of the parsing function.
 final class Parser {
 	private GlobalContext gCtx;
 	private NodeProgram program;
@@ -24,9 +25,105 @@ final class Parser {
 		return gCtx.toks[idx + amt];
 	}
 
+	/**
+	 * Checks the peeked token's kind, if it does not match
+	 * then it will print an error and (hopefully) exit.
+	 */
+	private void expect(TokenKind kind, string msg, ulong amt = 0) {
+		if (!peek(amt).isKind(kind)) {
+			Message tmp = new Message(
+				MessageKind.Error,
+				msg,
+				peek(amt)
+			);
+			tmp.display(gCtx);
+			return;
+		}
+	}
+	private void expect(TokenKind kind, string msg, string note, ulong amt = 0) {
+		if (!peek(amt).isKind(kind)) {
+			Message tmp = new Message(
+				MessageKind.Error,
+				msg,
+				note,
+				peek(amt)
+			);
+			tmp.display(gCtx);
+			return;
+		}
+	}
+	private void expect(TokenKind kind1, TokenKind kind2, string msg, ulong amt = 0) {
+		if (!peek(amt).isKind(kind1) && !peek(amt).isKind(kind2)) {
+			Message tmp = new Message(
+				MessageKind.Error,
+				msg,
+				peek(amt)
+			);
+			tmp.display(gCtx);
+			return;
+		}
+	}
+	private void expect(TokenKind kind1, TokenKind kind2, string msg, string note, ulong amt = 0) {
+		if (!peek(amt).isKind(kind1) && !peek(amt).isKind(kind2)) {
+			Message tmp = new Message(
+				MessageKind.Error,
+				msg,
+				note,
+				peek(amt)
+			);
+			tmp.display(gCtx);
+			return;
+		}
+	}
+
+	private NodeVar parseVar() {
+		idx++;
+		expect(
+			TokenKind.Identifier,
+			"Expected an identifier.",
+			"Identifiers start with a letter or an underscore."
+		);
+		NodeIdentifier ident = new NodeIdentifier(curr());
+		idx++;
+		expect(
+			TokenKind.Colon,
+			"Expected a colon.",
+			"Colons are required for definitions."
+		);
+		idx++;
+		expect(
+			TokenKind.Primitive, TokenKind.Identifier,
+			"Expected a primitive type or identifier.",
+			"Ensure that this is a valid identifier or primitive type."
+		);
+		NodeType type = new NodeType(curr());
+		idx++;
+		if (curr().isKind(TokenKind.Equ)) {
+			idx++;
+			NodeExpression expr = parseExpression(true);
+			return new NodeVar(ident, type, expr);
+		}
+		expect(
+			TokenKind.Semicolon,
+			"Expected a semicolon.",
+			"Semicolons delimit lines."
+		);
+		idx++;
+		return new NodeVar(ident, type);
+	}
+
+	private NodeExpression parseExpression(bool semicolon = false) {
+		NodeExpression expr;
+		return expr;
+	}
+
 	private NodeStatement parseStatement() {
 		NodeStatement statement;
 		switch (curr().kind) {
+			case TokenKind.Var: {
+				statement = parseVar();
+				break;
+			}
 			// Error!
 			default: {
 				Message msg = new Message(
